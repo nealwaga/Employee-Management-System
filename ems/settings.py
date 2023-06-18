353aff4699b5e884d07e11be493031d7b886d865
nealwaga <waganeal@gmail.com>
2023-06-18 22:30:28 +0300

    successfully connected to postgres

--- ems/settings.py
+++ ems/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+import dotenv
+dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'dashboard',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'ems.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField (User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to='media/profile_pics/', blank=True, null=True)
+    name = models.CharField (blank=False, max_length=100)
+    email = models.EmailField (blank=False, max_length=100)
+    job_title = models.CharField(max_length=100, blank=True, null=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+    @property
+    def username(self):
+        return self.user.username
+
+    def __str__(self):
+        return f'{self.user.username} - Profile'
+
+    # def __str__(self):
+    #     return self.user
+
+
+    @receiver (post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver (post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
