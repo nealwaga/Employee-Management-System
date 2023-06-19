9d2fcc02d4fbc2d8c799b76d7a85da9e5f684096
nealwaga <waganeal@gmail.com>
2023-06-19 15:59:31 +0300

    fixed bug

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UploadPDF(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_pdf')
     subject = models.CharField(max_length=50, blank=True, null=True)
-    pdf_file = models.FileField(upload_to='media/pdfs/')
+    pdf_file = models.FileField(upload_to='media/files/')
     upload_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
